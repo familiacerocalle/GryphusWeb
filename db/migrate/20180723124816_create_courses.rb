325495f834b027f7becf7b40bec8f507884b1782
Dilkhush Soni <dilkhushsoni2010@gmail.com>
2018-10-20 21:49:54 +0530

    changes for views

--- db/migrate/20180723124816_create_courses.rb
+++ db/migrate/20180723124816_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
       t.string :nombre
-      t.string :contenido
+      t.text :contenido
       t.integer :puntos
 
       t.timestamps
